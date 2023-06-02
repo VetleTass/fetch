60cb89d4a5a48bd8392e6a721cd3e8b00d606107
Vetle <103029390+VetleTass@users.noreply.github.com>
2023-06-02 10:48:17 +0200

    la til exception og try
    det kommer flere exceptions

--- fetch.py
+++ fetch.py
@@ -20,3 +20,15 @@ koammandoer = [
 ssh_client = paramiko.SSHClient()
 #her tar vi å auto godkjenner når vi ssh'er 
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+
+#her setter vi en while løkka slik at den 
+while True:
+    try:
+        # her kobler vi oss til ipen og ssh porten med bruker navn og passord 
+        ssh_client.connect(TARGET_IP, SSH_PORT, BRUKERNAVN, PASSORD)
+        
+    # denne her legger vi til i tilfelle noe går galt så ser vi hvor det skjedde 
+    except paramiko.AuthenticationException:
+        print("bruker navn og/eller passord er feil")
+    
